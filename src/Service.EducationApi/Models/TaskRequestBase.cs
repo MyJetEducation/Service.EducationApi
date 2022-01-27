5611e34792053789eb9836dd4f166ef350900c86
dmitriy.b <dmitriy.b@smplt.net>
2022-01-27 10:13:21 +0500

    Additional description to task params

--- src/Service.EducationApi/Models/TaskRequestBase.cs
+++ src/Service.EducationApi/Models/TaskRequestBase.cs
@@ -6,9 +6,12 @@ namespace Service.EducationApi.Models
 	public class TaskRequestBase
 	{
 		[Required]
+		[DefaultValue(false)]
 		public bool IsRetry { get; set; }
 
 		[Required]
+		[Range(1, int.MaxValue)]
+		[DefaultValue(0)]
 		[Description("Milliseconds")]
 		public int Duration { get; set; }
 	}
